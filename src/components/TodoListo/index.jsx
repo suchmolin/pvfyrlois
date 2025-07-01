0da6a0d1f5e04834e64e32d3bf28543d25f21ae7
suchmolin11 <suchmolin11@gmail.com>
2025-06-30 23:00:39 -0400

    exploracion, deporte, tecnologia conoce la ciudad

--- src/components/TodoListo/index.jsx
+++ src/components/TodoListo/index.jsx
@@ -1,8 +1,111 @@
+import { FiArrowRightCircle } from "react-icons/fi";
+
 export default function TodoListo() {
     return (
         <section className="w-full flex flex-col items-center">
             <h2 className="text-center text-lg text-azul">¡Todo listo para que tu hijo viva <br />
-                las mejores aventuras <b>en inglés este verano!</b></h2>
-        </section>
+                las mejores aventuras <b>en inglés este verano!</b>
+            </h2>
+            <div className="w-full flex justify-center py-5">
+                <div className="flex flex-wrap gap-4 justify-center">
+                    <a href="#exploracion" className="w-[150px] rounded-md overflow-hidden hover:scale-[102%] duration-300 cursor-pointer">
+                        <img src="/img/exploracion.webp" alt="exploracion imagen" />
+                    </a>
+                    <a href="#deporte" className="w-[150px] rounded-md overflow-hidden hover:scale-[102%] duration-300 cursor-pointer">
+                        <img src="/img/deporte.webp" alt="deporte imagen" />
+                    </a>
+                    <a href="#tecnologia" className="w-[150px] rounded-md overflow-hidden hover:scale-[102%] duration-300 cursor-pointer">
+                        <img src="/img/tecnologia.webp" alt="tecnologia imagen" />
+                    </a>
+                    <a href="#conoceciudad" className="w-[150px] rounded-md overflow-hidden hover:scale-[102%] duration-300 cursor-pointer">
+                        <img src="/img/conoceciudad.webp" alt="conoceciudad imagen" />
+                    </a>
+                </div>
+            </div>
+            <div id="exploracion" className="w-full py-10 flex gap-4 justify-center items-center relative text-white">
+                <img src="/img/exploracionfondo.webp" alt="figura de fondo para exploracion" className="w-full h-full absolute top-0 left-0" />
+                <div className="w-[200px] min-[350px]:w-[250px] relative text-xs min-[350px]:text-sm">
+                    <h3 className="font-bold">EXPLORACIÓN</h3>
+                    <div>
+
+                        <p>Conocerá su lado más <b>aventurero </b>
+                            de una forma diferente y divertida.
+                        </p>
+                    </div>
+                </div>
+                <div className="w-[75px] flex flex-wrap gap-1 relative">
+                    <img src="/img/globo.webp" alt="globo terraqueo" className="w-[30px] min-[350px]:w-[35px] aspect-square" />
+                    <img src="/img/franjas.webp" alt="globo terraqueo" className="w-[30px] min-[350px]:w-[35px] aspect-square" />
+                    <img src="/img/montannas.webp" alt="globo terraqueo" className="w-[30px] min-[350px]:w-[35px] aspect-square" />
+                    <img src="/img/linterna.webp" alt="globo terraqueo" className="w-[30px] min-[350px]:w-[35px] aspect-square" />
+                </div>
+            </div>
+            <div id="deporte" className="w-full py-10 flex gap-4 justify-center items-center relative text-azul">
+                <img src="/img/deportefondo.webp" alt="figura de fondo para exploracion" className="w-full h-full absolute top-0 left-0" />
+                <div className="w-[200px] min-[350px]:w-[250px] relative text-xs min-[350px]:text-sm">
+                    <h3 className="font-bold">DEPORTE</h3>
+                    <div>
+                        <p>
+                            Vivirá una experiencia llena de
+                            energía a través del <b>deporte y
+                                actividades físicas.</b>
+                        </p>
+                    </div>
+                </div>
+                <div className="w-[75px] flex flex-wrap gap-1 relative">
+                    <img src="/img/escudoe.webp" alt="globo terraqueo" className="w-[30px] min-[350px]:w-[35px] aspect-square" />
+                    <img src="/img/control.webp" alt="globo terraqueo" className="w-[30px] min-[350px]:w-[35px] aspect-square" />
+                    <img src="/img/virtual.webp" alt="globo terraqueo" className="w-[30px] min-[350px]:w-[35px] aspect-square" />
+                    <img src="/img/corona.webp" alt="globo terraqueo" className="w-[30px] min-[350px]:w-[35px] aspect-square" />
+                </div>
+            </div>
+            <div id="tecnologia" className="w-full py-10 flex gap-4 justify-center items-center relative text-azul">
+                <img src="/img/tecnologiafondo.webp" alt="figura de fondo para exploracion" className="w-full h-full absolute top-0 left-0" />
+                <div className="w-[200px] min-[350px]:w-[250px] relative text-xs min-[350px]:text-sm">
+                    <h3 className="font-bold">TECNOLOGÍA</h3>
+                    <div>
+                        <p>
+                            Hará un recorrido a través de
+                            la <b>tecnología</b> y el <b>mundo actual.</b>
+
+                        </p>
+                    </div>
+                </div>
+                <div className="w-[75px] flex flex-wrap gap-1 relative">
+                    <img src="/img/ia.webp" alt="globo terraqueo" className="w-[30px] min-[350px]:w-[35px] aspect-square" />
+                    <img src="/img/virtual.webp" alt="globo terraqueo" className="w-[30px] min-[350px]:w-[35px] aspect-square" />
+                    <img src="/img/planeta.webp" alt="globo terraqueo" className="w-[30px] min-[350px]:w-[35px] aspect-square" />
+                    <img src="/img/manorobotica.webp" alt="globo terraqueo" className="w-[30px] min-[350px]:w-[35px] aspect-square" />
+                </div>
+            </div>
+            <div id="conoceciudad" className="w-full py-10 flex gap-4 justify-center items-center relative text-white">
+                <img src="/img/conoceciudadfondo.webp" alt="figura de fondo para exploracion" className="w-full h-full absolute top-0 left-0" />
+                <div className="w-[200px] min-[350px]:w-[250px] relative text-xs min-[350px]:text-sm">
+                    <h3 className="font-bold">CONOCE LA CIUDAD</h3>
+                    <div>
+
+                        Se sumergirá en <b>la magia de la
+                            ciudad,</b> descubriendo la historia
+                        que la hace brillar.
+
+                    </div>
+                </div>
+                <div className="w-[75px] flex flex-wrap gap-1 relative">
+                    <img src="/img/globo.webp" alt="globo terraqueo" className="w-[30px] min-[350px]:w-[35px] aspect-square" />
+                    <img src="/img/formas.webp" alt="globo terraqueo" className="w-[30px] min-[350px]:w-[35px] aspect-square" />
+                    <img src="/img/lupa.webp" alt="globo terraqueo" className="w-[30px] min-[350px]:w-[35px] aspect-square" />
+                    <img src="/img/monumento.webp" alt="globo terraqueo" className="w-[30px] min-[350px]:w-[35px] aspect-square" />
+                </div>
+            </div>
+            <div className="w-full pt-15 pb-10 flex items-center justify-center bg-[url('/img/papel.webp')] bg-center bg-cover -mt-5">
+                <div className="flex justify-center hover:scale-[102%] duration-300">
+                    <a href="#" className="flex text-naranja text-nowrap text-lg min-[375px]:text-xl min-[430px]:text-2xl font-eastman-bold items-center justify-center gap-2 bg-azul py-2 px-2 rounded-md mb-5 w-fit">
+                        <span className="text-white text-4xl"><FiArrowRightCircle /></span>
+                        <div className="h-8 w-[2px] rounded-md bg-white">{" "}</div>
+                        <span className="-mt-1">¡INSCRÍBELO AQUÍ!</span>
+                    </a>
+                </div>
+            </div>
+        </section >
     )
 }
