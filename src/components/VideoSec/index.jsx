77eb28e4fcc891b86cb46d6b8c74d1319d8377cf
suchmolin11 <suchmolin11@gmail.com>
2025-07-02 23:21:53 -0400

    agregar video principal

--- src/components/VideoSec/index.jsx
+++ src/components/VideoSec/index.jsx
@@ -4,7 +4,7 @@ export default function VideoSec() {
 
             <div className="relative py-4 sm:py-6 px-6 w-11/12 sm:w-[640px] lg:w-[850px]">
                 <img src="/img/formaVideo.webp" alt="fondo video" className="absolute top-0 left-0 w-full h-full" />
-                <div className="bg-gray-400 w-full aspect-video relative"></div>
+                <video className="w-full aspect-video relative" src="/img/videolandingsummer.mp4" controls />
             </div>
         </div>
     )
