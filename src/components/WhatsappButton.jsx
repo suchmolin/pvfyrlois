e8be6c60ca0e93925efaba63be9324c8699f2434
suchmolin11 <suchmolin11@gmail.com>
2025-07-03 13:53:15 -0400

    correccion numero de whatsapp

--- src/components/WhatsappButton.jsx
+++ src/components/WhatsappButton.jsx
@@ -3,7 +3,7 @@ export default function WhatsappButton() {
   return (
     <a
       aria-label="Whatsapp"
-      href="https://wa.me/message/584242760113"
+      href="https://wa.me/584242760113"
       target="_blank"
       className="fixed bottom-4 right-4 p-2 rounded-full text-white bg-[#309B42] text-2xl z-50 hover:scale-105 transition-all duration-300"
     >
