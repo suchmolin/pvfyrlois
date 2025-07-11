c062b0cd4f407ef5d981c967a5989f5efd601519
suchmolin11 <suchmolin11@gmail.com>
2025-07-10 22:22:31 -0400

    fix fuente de numeros

--- src/components/Caracteristicas/index.jsx
+++ src/components/Caracteristicas/index.jsx
@@ -21,7 +21,7 @@ export default function Caracteristicas() {
             <div className="flex justify-center mb-7">
                 <div className="relative p-2 md:p-4 xl:p-6 w-fit">
                     <img src="/img/fondohorario.webp" alt="fondo horario" className="w-full h-full absolute top-0 left-0" />
-                    <div className="text-[#001A70] text-nowrap text-sm min-[360px]:text-base min-[395px]:text-lg min-[480px]:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-lemon-bold relative flex items-center justify-center gap-2">
+                    <div className="text-[#001A70] text-nowrap text-sm min-[360px]:text-base min-[395px]:text-lg min-[480px]:text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl lemon-bold relative flex items-center justify-center gap-2">
                         9:00 - 12:00 P.M.
                         <div className="w-[2px] h-[20px] bg-[#001A70] mt-1"></div>
                         3:00 - 6:00 P.M.
